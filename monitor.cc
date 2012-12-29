acfcdad101ef70fe0c4fb658c82cb2cb358bece5
Devraj Mehta <devraj@cosmicac.(none)>
2012-12-29 02:19:49 -0500

    updates (lol worst commit msg ever?)

--- monitor.cc
+++ monitor.cc
@@ -28,7 +28,7 @@ extern "C" {
 using namespace std;
 
 //Local Environment variables
-string defaultReqDir("/home/devraj/Dropbox/cat-pictures/"); //req dir
+string request("/home/devraj/Dropbox/cat-pictures/"); //req dir
 string listfname("list.txt"); //filename for list in req dir
 string archive("/home/devraj/Videos/"); //archive dir
 
@@ -106,26 +106,17 @@ int main( const int argc , const char** argv ){
 		return( 1 ) ;
 	}
 	
-	
 	//build watch list of archive directories
-	
+	//TODO ^
 	
 	//register the archive directories with fam
 	for(DirList::const_iterator ix( archiveDirs.begin() ) , stop( archiveDirs.end() ) ; ix != stop ; ++ix){
 		register_directory( reinterpret_cast< string* >( *ix ) );
 	}	
 	
-	//register default request dir if none are provided
+	//register the request dir
 	if( 1 == argc ){
-		cout << "Defaulting request dir to " << dropbox << endl ;
-		register_directory( &dropbox );
-	}
-	
-	//register request directories from passed in arguments
-	for( int ix = 1 ; ix < argc ; ++ix ){
-		string* dir = new string( argv[ix] ) ;
-		register_directory( dir );
-		requestDirs.push_back( dir ) ; // all inputed dirs are dropbox dirs
+		register_directory( &request );
 	}
 	
 	//check to make sure there are connected dirs to monitor
@@ -153,7 +144,7 @@ int main( const int argc , const char** argv ){
 		
 		if( archive == *dir ){ //TODO change this to check list of archiveDirs (add loop and flag)
 			if( fe->code == FAMChanged || fe->code == FAMCreated || fe->code == FAMDeleted ){
-				//update list.txt in dropbox dir //TODO move this to method updateList( listfname, archive )
+				//update list.txt in req dir //TODO move this to method updateList( listfname, archive )
 				string update( "ls " );
 				update = update + archive + " > " + dropbox + listfname; //TODO concatenate all the different archive dirs together
 				system( update.c_str() );
@@ -162,103 +153,56 @@ int main( const int argc , const char** argv ){
 			}
 		}
 		else { //if not archive dir assume request dir
-			switch( fe->code ){ //TODO maybe change this to an if since we only care about FAMCreated
-				case FAMCreated:
-					if(createsTriggered > 0) { //skip self triggered events
-						createsTriggered -= 1;
-						cout << " skipped create. creates left: " << createsTriggered << endl ;
-						continue;
-					}
-					if(fname == listfname) { //hey! no changing this file
-						//TODO call updateList( listfname, archive )
-						continue; //TODO 
-					}
-					cout << "DIR: dropbox ";
-					cout << "CREATED " ;
-					cout << "Event on watched dir \"" << *dir << "\", file \"" << fname << "\" : " ;
-					cout << "file was created" ;
-					cout << endl ;
-					
-					if( 0 != access(  archfile.c_str() , F_OK ) ){
-						cerr << archfile << " file access problem" ;
-					}
-					system( ("cp -f " + archfile + " " + dropbox + fname).c_str() );
-					createsTriggered += 1;
-					cout << createsTriggered << " creates triggered" << endl;
-					
-					//TODO setup callback to delete the file from dropbox in approx 2hrs
-					
-					//TODO add capability to accept uploaded user content 
-					
-					break ;
-					
-				case FAMChanged:
-					//either this script responding to a request
-					//or a user renaming a request
-					//note: the number of these per added file 
-					//      depends on the number of writes the 
-					//      the filesystem  makes (e.g. fsize)
-					/* abandoned for now due to above note
-					cout << "DIR: dropbox ";
-					cout << "CHANGED " ;
-					cout << "Event on watched dir \"" << *dir << "\", file \"" << fe->filename << "\" : " ;
-					cout << "file has changed" ;
-					cout << endl ;
-					*/
-					break ;
-					
-
-				case FAMDeleted:
-					//XXX caution this seems to be double sending some delete signals
-					/* abandoned for now due to above note
-					cout << "DIR: dropbox ";
-					cout << "DELETED " ;
-					cout << "Event on watched dir \"" << *dir << "\", file \"" << fe->filename << "\" : " ;
-					cout << "file was removed" ;
-					cout << endl ;
-					*/
-					break ;
+			if( fe->code == FAMCreated ){ // we only care about creation events in the req dir
+				if(createsTriggered > 0) { //skip self triggered events
+					createsTriggered -= 1;
+					cout << " skipped create. creates left: " << createsTriggered << endl ;
+					continue;
+				}
+				if(fname == listfname) { //hey! no changing this file
+					//TODO call updateList( listfname, archive )
+					continue; //TODO 
+				}
+				cout << "DIR: request ";
+				cout << "CREATED " ;
+				cout << "Event on watched dir \"" << *dir << "\", file \"" << fname << "\" : " ;
+				cout << "file was created" ;
+				cout << endl ;
 				
-			} // switch( fe->code )
+				if( 0 != access(  archfile.c_str() , F_OK ) ){
+					cerr << archfile << " file access problem" ;
+				}
+				system( ("cp -f " + archfile + " " + request + fname).c_str() );
+				createsTriggered += 1;
+				cout << createsTriggered << " creates triggered" << endl;
+			}
 		}
-		
-		//cout << endl ;
-		
-	}
+	} //while( runFam )
 
 	// - - - - - - - - - - - - - - - - - - - -
 	// cleanup
-
 
 	for(
 		RequestList::const_iterator ix( requests.begin() ) , stop( requests.end() ) ;
 		ix != stop ;
 		++ix
 	){
-
 		cout << "[Cancelling monitor for FAMRequest " << (*ix)->reqnum << "]" << endl ;
 		FAMCancelMonitor( fc , *ix ) ;
-
 		delete( *ix ) ;
-
 	}
-
 
 	for(
 		DirList::const_iterator iy( dirNames.begin() ) , stop( dirNames.end() ) ;
 		iy != stop ;
 		++iy
 	){
-
 		cout << "deleting dir " << (*iy)->c_str() << endl ;
 		delete( *iy ) ;
-
 	}
-
 
 	// disconnect from the FAM service
 	FAMClose( fc ) ;
-
 	delete( fe ) ;
 	delete( fc ) ;
 
